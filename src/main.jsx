431833867e9e73aa7af74f4a200a4167bf4c132b
jagoraxm <jagoraxr.moucha@gmail.com>
2024-06-06 03:08:05 -0600

    Feature: Change front style

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,12 @@ import './index.css'
 import { Provider } from 'react-redux'
 import {store} from './store'
 
+import { ThemeProvider } from "@material-tailwind/react";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </Provider>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
@@ -11,5 +14,5 @@ export default {
   plugins: [
     require('flowbite/plugin')
   ]
-}
+})
 
